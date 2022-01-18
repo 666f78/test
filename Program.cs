32a33ab926c6203226e00c84509c05c79fd5f565
666f78 <dr.kot3@gmail.com>
2022-01-18 16:04:04 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,4 @@
 ﻿using HtmlAgilityPack;
-using OpenQA.Selenium.Appium;
-using OpenQA.Selenium.Appium.Windows;
-using System;
 using System.Text;
 
 namespace ParseMusicSite
@@ -29,49 +26,3 @@ namespace ParseMusicSite
 
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//static void AppParse()
-//{
-//    WindowsDriver<WindowsElement> notepad;
-//    AppiumOptions des = new AppiumOptions();
-//    des
-//        .AddAdditionalCapability("app", @"C:\Windows\System32\notepad.exe");
-
-
-//    Thread.Sleep(5);
-//    notepad = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), des);
-
-//    //\"]/Pane[@Name=\"Google Chrome\"]/Pane/Pane[position()=1]/Pane/Tab/Pane/TabItem[starts-with(@Name,\"5. WinAppDriver Tutorial | How To Set Up WinAppDriver in Windows\")]/Button[@Name=\"Close\"]"
-//}
